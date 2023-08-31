dbead5b5a20c3fcd068b60715ecc1a03adedb93f
Eric Nelson <ernelson12o0@gmail.com>
2023-08-31 13:42:04 -0400

    Lab Complete

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,14 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, anagrams):
+        matching_anagrams = []
+        sorted_word = sorted(self.word.lower())
+
+        for anagram in anagrams:
+            if sorted(anagram.lower()) == sorted_word and anagram.lower() != self.word.lower():
+                matching_anagrams.append(anagram)
+        
+        return matching_anagrams
+           
